316877dbe0112c224fa2ea7463a0ca5f93699b16
Jeffrey <Jeffogya@gmail.com>
2013-09-16 11:44:22 +0200

    updatetje

---
+++ Les 2/opdracht 5.js
@@ -0,0 +1,65 @@
+var GEO = GEO || {};
+
+var GEO.controller = {};
+
+(function() {
+// The gps object
+var GEO.gps = {	
+	init: function () {
+
+	},
+	
+	startInterval: function () {
+	
+	},
+	
+	updatePosition: function () {
+	
+	},
+	
+	setPosition: function () {
+	
+	},
+	
+	checkLocations: function () {
+		
+	},
+	
+	calculateDistance: function () {
+	
+	}
+};
+
+
+
+// The map object
+var GEO.map = {
+	generateMap: function () {	
+
+	},
+
+	isNumber: function () {
+
+	},
+
+	updatePosition: function () {
+
+	}
+};
+
+// The debug object
+var GEO.debug = {
+	geoErrorHandler: function () {
+	
+	},
+
+	message: function () {
+	
+	},
+
+	setCustomDebugging: function () {
+
+	}
+};
+
+})();
