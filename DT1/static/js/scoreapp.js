51032e66561323ea8726964c53b8d2f95c66e99e
Jeffrey <Jeffogya@gmail.com>
2013-10-28 10:41:09 +0100

    worked on it

--- DT1/static/js/scoreapp.js
+++ DT1/static/js/scoreapp.js
@@ -2,70 +2,61 @@
 var SCOREAPP = SCOREAPP || {};
 
 (function () {
-
+	
 	// Data
 	SCOREAPP.schedule = {
-		title:'Pool A - Schedule',
-		items: [
-		    { date: "Monday, 9:00am", team1: "Chasing", team1Score: "13", team2: "Amsterdam Money Gang", team2Score: "9"},
-		    { date: "Monday, 9:00am", team1: "Boomsquad", team1Score: "15", team2: "Beast Amsterdam", team2Score: "11"},
-		    { date: "Monday, 10:00am", team1: "Beast Amsterdam", team1Score: "14", team2: "Amsterdam Money Gang", team2Score: "12"},
-		    { date: "Monday, 10:00am", team1: "Chasing", team1Score: "5", team2: "Burning Snow", team2Score: "15"},
-		    { date: "Monday, 11:00am", team1: "Boomsquad", team1Score: "11", team2: "Amsterdam Money Gang", team2Score: "15"},    
-		    { date: "Monday, 11:00am", team1: "Burning Snow", team1Score: "15", team2: "Beast Amsterdam", team2Score: "6"},
-		    { date: "Monday, 12:00pm", team1: "Chasing", team1Score: "8", team2: "Beast Amsterdam", team2Score: "15"},
-		    { date: "Monday, 12:00pm", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"},
-		    { date: "Monday, 1:00pm", team1: "Chasing", team1Score: "15", team2: "Boomsquad", team2Score: "14"},
-		    { date: "Monday, 1:00pm", team1: "Burning Snow", team1Score: "15", team2: "Amsterdam Money Gang", team2Score: "11"}
-	    ]
+		title: "Schedule",
+		start: function() {
+			jx.load('https://api.leaguevine.com/v1/games/?pool_id=19219&access_token=9e0cf40d3d', function(data) {
+				var data = JSON.parse(data);
+				console.log(data);
+				var directives = { //Verander de manier van data-binden
+					objects: { resource_uri: {
+						href: function(){
+							return this.resource_uri;
+						},
+						html: function(){
+							return "Update Game";
+						}
+					}}
+				};
+			Transparency.render(qwery('[data-route=schedule]')[0], data, directives);
+			},'text','get'); // Haal data op als text.
+		}
 	};
 
 	SCOREAPP.ranking = {
-		title:'Pool A - Ranking',
-		items: [
-		    { team: "Chasing", win: "2", lost: "2", sw: "7", sl: "9", pw: "35", pl: "39"},
-		    { team: "Boomsquad", win: "2", lost: "2", sw: "9", sl: "8", pw: "36", pl: "34"},
-		    { team: "Burning Snow", win: "3", lost: "1", sw: "11", sl: "4", pw: "36", pl: "23"},
-		    { team: "Beast Amsterdam", win: "2", lost: "2", sw: "6", sl: "8", pw: "30", pl: "34"},
-		    { team: "Amsterdam Money Gang", win: "1", lost: "3", sw: "6", sl: "10", pw: "30", pl: "37"}
-	    ]
+		title: "Ranking pool A",
+		description: "Dit zijn de rankings voor pool A",
+		start: function() {
+			jx.load('https://api.leaguevine.com/v1/pools/19219/', function(data) {
+				var data = JSON.parse(data);
+				console.log(data);
+				var directives = { //Verander de manier van data-binden
+				};	
+			Transparency.render(qwery('[data-route=ranking]')[0], data, directives);
+			},'text','get'); // Haal data op als text.
+		}
 	};
 
 	SCOREAPP.game = {
-		title: 'Pool A - Score: Boomsquad vs. Burning Snow',
-		description: 'Tournament Games',
-		items: [
-		    { score: "1", team1: "Boomsquad", team1Score: "1", team2: "Burning Snow", team2Score: "0"},
-		    { score: "2", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "0"},
-		    { score: "3", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "1"},
-		    { score: "4", team1: "Boomsquad", team1Score: "2", team2: "Burning Snow", team2Score: "2"},
-		    { score: "5", team1: "Boomsquad", team1Score: "3", team2: "Burning Snow", team2Score: "2"},
-		    { score: "6", team1: "Boomsquad", team1Score: "4", team2: "Burning Snow", team2Score: "2"},
-		    { score: "7", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "2"},
-		    { score: "8", team1: "Boomsquad", team1Score: "5", team2: "Burning Snow", team2Score: "3"},
-		    { score: "9", team1: "Boomsquad", team1Score: "6", team2: "Burning Snow", team2Score: "3"},
-		    { score: "10", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "3"},
-		    { score: "11", team1: "Boomsquad", team1Score: "7", team2: "Burning Snow", team2Score: "4"},
-		    { score: "12", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "4"},
-		    { score: "13", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "5"},
-		    { score: "14", team1: "Boomsquad", team1Score: "8", team2: "Burning Snow", team2Score: "6"},
-		    { score: "15", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "6"},
-		    { score: "16", team1: "Boomsquad", team1Score: "9", team2: "Burning Snow", team2Score: "7"},
-		    { score: "17", team1: "Boomsquad", team1Score: "10", team2: "Burning Snow", team2Score: "7"},
-		    { score: "18", team1: "Boomsquad", team1Score: "11", team2: "Burning Snow", team2Score: "7"},
-		    { score: "19", team1: "Boomsquad", team1Score: "12", team2: "Burning Snow", team2Score: "7"},
-		    { score: "20", team1: "Boomsquad", team1Score: "13", team2: "Burning Snow", team2Score: "7"},
-		    { score: "21", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "7"},
-		    { score: "22", team1: "Boomsquad", team1Score: "14", team2: "Burning Snow", team2Score: "8"},
-		    { score: "23", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"}
-		]
+		start: function() {
+			jx.load('https://api.leaguevine.com/v1/games/127162/', function(data) {
+				var data = JSON.parse(data);
+				console.log(data);
+				var directives = { //Verander de manier van data-binden
+				};	
+			Transparency.render(qwery('[data-route=game]')[0], data, directives);
+			},'text','get'); // Haal data op als text.
+		}
 	};
+
 
 	//Controller Init
 	SCOREAPP.controller = {
 		start: function () {
 			//log to check if the SCOREapplication has come this far
-			console.log("Start score applicatie");
+			console.log("Step 2: Start score application");
 
 			//pull data: schedule, game, ranking
 			SCOREAPP.router.start();
@@ -75,16 +66,13 @@ var SCOREAPP = SCOREAPP || {};
 	//Router
 	SCOREAPP.router = {
 		start: function () {
+			console.log("Step 3: In de html wordt er doormiddel van links ");
 	  		routie({
 			    '/schedule': function() {
 			    	SCOREAPP.content.render('schedule');
-				},
+			    },
 			    '/ranking': function() {
 			    	SCOREAPP.content.render('ranking');
-			    },
-
-			    '/game': function() {
-			    	SCOREAPP.content.render('game');
 			    },
 			    '*': function() {
 			    	SCOREAPP.content.render('schedule');
@@ -93,6 +81,7 @@ var SCOREAPP = SCOREAPP || {};
 		},
 
 		different: function () {
+			console.log("Step 5: Goes through the DOM searching for the defined emelents. If the right section is found it wil recieve a class active. All the other section will lose their active class.");
             var route = window.location.hash.slice(2),
                 sections = qwery('section'),
                 section = qwery('[data-route=' + route + ']')[0];
@@ -116,18 +105,17 @@ var SCOREAPP = SCOREAPP || {};
 	// Pages
 	SCOREAPP.content = {
 		render: function (route) {
+			console.log("Step 4: Data van de bijbehorende pagina wordt opgehaald afhangend van wat route is. route wordt vastgesteld in stap 3");
 			var data;
 			switch (route){
 				case 'schedule': 
-				data = SCOREAPP.schedule;
+				SCOREAPP.schedule.start();
+				SCOREAPP.router.different();
 				break;
-				
-				case 'ranking': 
-				data = SCOREAPP.ranking;
-				break;
-				
-				case 'game': 
-				data = SCOREAPP.game;
+
+				case 'ranking':
+				SCOREAPP.ranking.start();
+				SCOREAPP.router.different();
 				break;
 
 				default : 
@@ -142,9 +130,10 @@ var SCOREAPP = SCOREAPP || {};
 
 	//DOM ready code
 	domready(function(){
+		console.log("Step 1: DOM is ready");
 		//start SCOREapplication
 		SCOREAPP.controller.start();
-		console.log("dom is ready")
+		
 	});
 
 })();
